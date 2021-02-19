6e35c8f539f92d7a394e9bae34e247286e6d4b63
soroushamdg <soroushmpour@gmail.com>
2021-02-19 22:06:24 +0330

    updated pubspec.yaml + added configs

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todoey/models/tasks_provider.dart';
 import 'package:todoey/screens/tasks_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays(
+      [SystemUiOverlay.bottom, SystemUiOverlay.top]);
   runApp(MyApp());
 }
 
